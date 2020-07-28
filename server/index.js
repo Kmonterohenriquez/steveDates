e5a27a4ca7ae21a9e4d36869276e168e1fe76606
Kevin Montero <kevinwilding1997@gmail.com>
2020-07-28 11:19:27 -0400

    solution for MIME error

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ const express = require("express"),
   } = process.env,
   app = express();
 app.use(express.json());
+app.use(express.static(__dirname + '/public' ));
 app.use(express.static(`${__dirname}/../build`));
 // io = socket(
 //   app.listen(SERVER_PORT, () =>
